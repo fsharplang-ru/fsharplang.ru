62b5d18ca381c3e0deb176529610da3ed619cfa3
Roman Melnikov <iam@neftedollar.com>
2017-05-28 23:34:47 +0300

    подготовка  к   fable

--- src/client/src/App.fs
+++ src/client/src/App.fs
@@ -1,19 +1,84 @@
-module FSharplang_ru.Client
+module App.View
 
+open Elmish
+open Elmish.Browser.Navigation
+open Elmish.Browser.UrlParser
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Import.Browser
+open Types
+open App.State
+open Global
 
-let init() =
-    let canvas = Browser.document.getElementsByTagName_canvas().[0]
-    canvas.width <- 1000.
-    canvas.height <- 800.
-    let ctx = canvas.getContext_2d()
-    // The (!^) operator checks and casts a value to an Erased Union type
-    // See http://fable.io/docs/interacting.html#Erase-attribute
-    ctx.fillStyle <- !^"rgb(200,0,0)"
-    ctx.fillRect (10., 10., 55., 50.)
-    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
-    ctx.fillRect (30., 30., 55., 50.)
+importAll "../css/font-awesome.min.css"
+importAll "../css/bootstrap.css"
+importAll "../css/additional.css"
+importAll "../css/default.css" 
 
-init()
+
+
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+
+let menuItem label page currentPage =
+    li
+      [ ]
+      [ a
+          [ classList [ "is-active", page = currentPage ]
+            Href (toHash page) ]
+          [ str label ] ]
+
+let menu currentPage =
+  aside
+    [ ClassName "menu" ]
+    [ p
+        [ ClassName "menu-label" ]
+        [ str "General" ]
+      ul
+        [ ClassName "menu-list" ]
+        [ menuItem "Home" Home currentPage
+          menuItem "Counter sample" Counter currentPage
+          menuItem "About" Page.About currentPage ] ]
+
+let root model dispatch =
+
+  let pageHtml =
+    function
+    | Page.About -> Info.View.root
+    | Counter -> Counter.View.root model.counter (CounterMsg >> dispatch)
+    | Home -> Home.View.root model.home (HomeMsg >> dispatch)
+
+  div
+    []
+    [ div
+        [ ClassName "navbar-bg" ]
+        [ div
+            [ ClassName "container" ]
+            [ Navbar.View.root ] ]
+      div
+        [ ClassName "section" ]
+        [ div
+            [ ClassName "container" ]
+            [ div
+                [ ClassName "columns" ]
+                [ div
+                    [ ClassName "column is-3" ]
+                    [ menu model.currentPage ]
+                  div
+                    [ ClassName "column" ]
+                    [ pageHtml model.currentPage ] ] ] ] ]
+
+open Elmish.React
+open Elmish.Debug
+
+// App
+Program.mkProgram init update root
+|> Program.toNavigable (parseHash pageParser) urlUpdate
+|> Program.withReact "elmish-app"
+//-:cnd
+#if DEBUG
+|> Program.withDebugger
+#endif
+//+:cnd
+|> Program.run
